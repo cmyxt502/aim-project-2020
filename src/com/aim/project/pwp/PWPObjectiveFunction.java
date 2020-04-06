99c54d0ae14d53af70512dd1945c2b2ced6eee93
Maurice Chen <30866635+cmyxt502@users.noreply.github.com>
2020-04-07 00:09:39 +0100

    20200407
    getCost, getCostBetweenDepotAnd, getCostBetweenHomeAnd methods in PWPObjectiveFunction completed.

--- src/com/aim/project/pwp/PWPObjectiveFunction.java
+++ src/com/aim/project/pwp/PWPObjectiveFunction.java
@@ -1,5 +1,6 @@
 package com.aim.project.pwp;
 
+import com.aim.project.pwp.instance.Location;
 import com.aim.project.pwp.interfaces.ObjectiveFunctionInterface;
 import com.aim.project.pwp.interfaces.PWPInstanceInterface;
 import com.aim.project.pwp.interfaces.SolutionRepresentationInterface;
@@ -20,16 +21,34 @@ public class PWPObjectiveFunction implements ObjectiveFunctionInterface {
 	
 	@Override
 	public double getCost(int iLocationA, int iLocationB) {
-		
+		Location locationA = null;
+		Location locationB = null;
+		locationA = oInstance.getLocationForDelivery(iLocationA);
+		locationB = oInstance.getLocationForDelivery(iLocationB);
+		double diffX = Math.pow(locationA.getX() - locationB.getX(), 2);
+		double diffY = Math.pow(locationA.getY() - locationB.getY(), 2);
+		return Math.sqrt(diffX + diffY);
 	}
 
 	@Override
 	public double getCostBetweenDepotAnd(int iLocation) {
-		
+		Location locationA = null;
+		Location locationDepot = null;
+		locationA = oInstance.getLocationForDelivery(iLocation);
+		locationDepot = oInstance.getPostalDepot();
+		double diffX = Math.pow(locationA.getX() - locationDepot.getX(), 2);
+		double diffY = Math.pow(locationA.getY() - locationDepot.getY(), 2);
+		return Math.sqrt(diffX + diffY);
 	}
 
 	@Override
 	public double getCostBetweenHomeAnd(int iLocation) {
-		
+		Location locationA = null;
+		Location locationHome = null;
+		locationA = oInstance.getLocationForDelivery(iLocation);
+		locationHome = oInstance.getHomeAddress();
+		double diffX = Math.pow(locationA.getX() - locationHome.getX(), 2);
+		double diffY = Math.pow(locationA.getY() - locationHome.getY(), 2);
+		return Math.sqrt(diffX + diffY);
 	}
 }
